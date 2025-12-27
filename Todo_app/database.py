31ef34ffb1a40b0714c571c37f6d6fd55d8f1061
Zarin Saima Roza <zsroza333@gmail.com>
2025-12-27 19:20:13 +0600

    do

--- Todo_app/database.py
+++ Todo_app/database.py
@@ -5,6 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 SQLALCHEMY_DATABASE_URL = "sqlite:///./todo.db"
 
 
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args = {"check_same_thread" : False}) #fastapi works with multiple thread. Allows FastAPI (multi-threaded) to use the same SQLite connection safely
 
 session_local = sessionmaker(bind=engine,autoflush=False,auto_commit=False) #cause I want  to take the full control of the system autoflush=False → Changes are not sent to DB automatically autocommit=False → You must manually commit
 
