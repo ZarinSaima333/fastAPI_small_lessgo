ded11f20e33404b3f2e28d9a365368c60f3324d1
Zarin Saima Roza <zsroza333@gmail.com>
2025-12-27 18:23:45 +0600

    project todo_app

---
+++ Todo_app/database.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./todo.db"
+
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args = {"check_same_tread" : False}) #fastapi works with multiple thread. Allows FastAPI (multi-threaded) to use the same SQLite connection safely
+
+session_local = sessionmaker(bind=engine,autoflush=False,auto_commit=False) #cause I want  to take the full control of the system autoflush=False → Changes are not sent to DB automatically autocommit=False → You must manually commit
+
+Base = declarative_base() #Every table model will inherit from Base
+
+
