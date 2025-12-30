974cca5c3ba68f329d00efb90efbf72c48e5b5be
Zarin Saima Roza <zsroza333@gmail.com>
2025-12-31 03:37:15 +0600

    fixed
    fixed

--- Todo_app/database.py
+++ Todo_app/database.py
@@ -7,8 +7,8 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///./todo.db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args = {"check_same_thread" : False}) #fastapi works with multiple thread. Allows FastAPI (multi-threaded) to use the same SQLite connection safely
 
-session_local = sessionmaker(bind=engine,autoflush=False,auto_commit=False) #cause I want  to take the full control of the system autoflush=False → Changes are not sent to DB automatically autocommit=False → You must manually commit
+session_local = sessionmaker(bind=engine,autoflush=False) #cause I want  to take the full control of the system autoflush=False → Changes are not sent to DB automatically autocommit=False → You must manually commit
 
 Base = declarative_base() #Every table model will inherit from Base
 
-
+#,auto_commit=False eitai baad
--- Todo_app/main.py
+++ Todo_app/main.py
@@ -1,11 +1,31 @@
 #root
-
-from fastapi import FastAPI
+from typing import Annotated
+from sqlalchemy.orm import Session
+from fastapi import FastAPI, Depends
 import model
-from database import engine
+from model import Todo
+from database import engine, session_local
 
 app = FastAPI()
 
 model.Base.metadata.create_all(bind=engine)
 
 #after making this file we'll start the uvicorn command and automatically todo.db will be created
+
+def get_db():
+    db = session_local()
+    try:
+        yield db #yield shoho get_db porjonto jabe, er ager part, response pathanor ag porjonto, only uses db when using it, colses after, safe and fast.
+    finally:
+        db.close() #afyer the response has been deleivered
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@app.get("/")
+async def read_all(db: db_dependency):
+    return db.query(Todo).all()
+
+
+
+
