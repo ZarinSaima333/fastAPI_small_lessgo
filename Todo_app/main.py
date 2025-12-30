5117bc19504d92b2d7249dbbe57d8cb9557df0f7
Zarin Saima Roza <zsroza333@gmail.com>
2025-12-31 04:08:36 +0600

    Update main.py

--- Todo_app/main.py
+++ Todo_app/main.py
@@ -1,7 +1,7 @@
 #root
 from typing import Annotated
 from sqlalchemy.orm import Session
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends,HTTPException,status,Path
 import model
 from model import Todo
 from database import engine, session_local
@@ -22,10 +22,17 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
-@app.get("/")
+@app.get("/",status_code=status.HTTP_200_OK)
 async def read_all(db: db_dependency):
     return db.query(Todo).all()
 
 
+@app.get("/todo/{todo_id}",status_code=status.HTTP_200_OK)
+async def read_todo(db:db_dependency, todo_id:int=Path(gt=0)):
+    todo_model = db.query(Todo).filter(Todo.id== todo_id).first()
+    if todo_model is not None:
+        return todo_model
+    raise HTTPException(status_code=404,detail="Todo not found.")
 
+@
 
