a37ade78393c65a494acca5437564a33c97b4abc
Zarin Saima Roza <zsroza333@gmail.com>
2025-12-31 05:33:42 +0600

    api reqs

--- Todo_app/main.py
+++ Todo_app/main.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI, Depends,HTTPException,status,Path
 import model
 from model import Todo
 from database import engine, session_local
-
+from pydantic import BaseModel,Field
 app = FastAPI()
 
 model.Base.metadata.create_all(bind=engine)
@@ -22,6 +22,14 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
+#pydantic req: it is a todos req of a post http req of a body of a todo, validaton
+
+class TodoReq(BaseModel):
+    title:str = Field(min_length=3)
+    description:str = Field(min_length=3,max_length=100)
+    priority: int= Field(gt=0, lt=6)
+    complete:bool
+
 @app.get("/",status_code=status.HTTP_200_OK)
 async def read_all(db: db_dependency):
     return db.query(Todo).all()
@@ -34,5 +42,36 @@ async def read_todo(db:db_dependency, todo_id:int=Path(gt=0)):
         return todo_model
     raise HTTPException(status_code=404,detail="Todo not found.")
 
-@
+@app.post("/todo",status_code=status.HTTP_200_OK)
+async def create_todo(db:db_dependency,todo_req:TodoReq):
+    todo_model=Todo(**todo_req.model_dump())
+    db.add(todo_model)
+    db.commit()
 
+
+@app.put('/todo/{todo_id}',status_code=status.HTTP_204_NO_CONTENT)
+async def update_todo(db:db_dependency,
+                       todo_req:TodoReq,
+                      todo_id:int=Path(gt=0)#error if put before todo_req should be above of anything hat deala with a path paramter
+                     ):
+    
+    todo_model=db.query(Todo).filter(Todo.id==todo_id).first()
+    if todo_model is None:
+        
+        raise HTTPException(status_code=404,detail="Todo not found.")
+    todo_model.title = todo_req.title
+    todo_model.description = todo_req.description
+    todo_model.priority=todo_req.priority
+    todo_model.complete=todo_req.complete
+    
+    
+    db.add(todo_model)
+    db.commit()
+
+@app.delete("\todo\{todo_id}",status_code=status.HTTP_204_NO_CONTENT)
+async def delete_todo(db:db_dependency,todo_id:int=Path(gt=0)):
+    todo_model=db.query(Todo).filter(Todo.id==todo_id).first()
+    if todo_model is None:    
+        raise HTTPException(status_code=404,detail="Todo not found.")
+    db.query(Todo).filter(Todo.id==todo_id).delete()
+    db.commit()
