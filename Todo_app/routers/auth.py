6442ac5e0bd731d4e97b64706f7915ee64ff4158
Zarin Saima Roza <zsroza333@gmail.com>
2026-01-03 01:06:59 +0600

    db injection and able to save info in database

--- Todo_app/routers/auth.py
+++ Todo_app/routers/auth.py
@@ -1,9 +1,13 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI, APIRouter, Depends,HTTPException,status,Path
+from typing import Annotated
+from sqlalchemy.orm import Session
+from database import  session_local
 from pydantic import BaseModel
 from model import Users
 from passlib.context import CryptContext
 
 router =  APIRouter()
+
 
 bcrypt_context = CryptContext(schemes=['bcrypt'],deprecated='auto')
 
@@ -15,10 +19,22 @@ class CreateUserReq(BaseModel):
     password:str
     role:str
 
-@router.post("/auth/")
-async def create_user(create_user_req:CreateUserReq):
+#get db
+
+def get_db():
+    db = session_local()
+    try:
+        yield db #yield shoho get_db porjonto jabe, er ager part, response pathanor ag porjonto, only uses db when using it, colses after, safe and fast.
+    finally:
+        db.close() #afyer the response has been deleivered
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@router.post("/auth/",status_code=status.HTTP_201_CREATED)
+async def create_user(db:db_dependency,create_user_req:CreateUserReq):
     #create_user_model = Users(**create_user_req.model_dump())
-    #uporer ta use korbo na karon class create users e password ase, ar amr model,py e ase  hashed_pass= Column(String)
+    #uporer ta use korbo na karon class create users e password ase, ar amr model,py e ase  hashed_pass= Column(String) basically db er table er column er shathe pydantic naam mile nai.
     create_user_model = Users(
         email=create_user_req.email,
         username=create_user_req.username,
@@ -29,7 +45,10 @@ async def create_user(create_user_req:CreateUserReq):
         is_active=True #pydantic class e nai
 
     )
-    return create_user_model #correct
+
+    db.add(create_user_model) #ekhono post req er output null ashe, sqlite e dekhte pai cmd te,db te save hoise.
+    db.commit()
+    #return create_user_model #correct
 
     #return {'user': "authentication"} return charao possible, response body te 200 ashe
 
